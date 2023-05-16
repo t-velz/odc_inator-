26429ed0998659fbaec3dd40308b8b1aefd46197
milajg <133723123+milajg@users.noreply.github.com>
2023-05-16 11:11:09 -0400

    Add files via upload

---
+++ Commands.r
@@ -0,0 +1,36 @@
+#Git and powershell commands
+
+#for all Git commands, when typing them in the shell be sure to precede them with git so the shell knows what program they are from
+
+add #add a file to a Git repository 
+#if you want to add all files in a directory, use "." ex.) git add . 
+#if you want to add specific files in a directory, replace the "." with actual file names
+
+branch #create and delete branches
+
+clone #clone a repository into the current working directory, clone to your local machine
+
+
+config #configure your Git username and email address
+#ex. git config--global user.name "your_user_name"
+#    git config--global user.email "your_email"
+
+commit #commit changes to a Git repository 
+
+fetch #download objects from another repository 
+
+.gitignore #this is not a command, but a file that can be added to your repository to specify what files Git should ignore
+
+init #initialize a Git repository 
+
+log #show a repository's commit history 
+
+pull #fetch data from a remote repository 
+
+push #add committed changes to a remote Git repository, such as GitHub
+
+rm #remove files
+
+#powershell commands
+
+cd #change working directory 
