c0723d40a7d0e7013b4e142a9f7f5be9c2ed9891
milajg <133723123+milajg@users.noreply.github.com>
2023-05-22 14:09:18 -0400

    Commands.r

--- Commands.r
+++ Commands.r
@@ -3,34 +3,35 @@
 #for all Git commands, when typing them in the shell be sure to precede them with git so the shell knows what program they are from
 
 add #add a file to a Git repository 
-#if you want to add all files in a directory, use "." ex.) git add . 
-#if you want to add specific files in a directory, replace the "." with actual file names
+#if you want to add all files to a directory, use "." ex.) git add .
+#if you want to add specific files, use the file names
 
 branch #create and delete branches
 
-clone #clone a repository into the current working directory, clone to your local machine
+checkout #lists all local branches 
 
+clone #clone a repository into current working directory 
 
-config #configure your Git username and email address
-#ex. git config--global user.name "your_user_name"
-#    git config--global user.email "your_email"
-
-commit #commit changes to a Git repository 
-
-fetch #download objects from another repository 
-
-.gitignore #this is not a command, but a file that can be added to your repository to specify what files Git should ignore
+config #configure your GitHub username and email, this can be used for adding files to a Git repository 
+#ex.) git config --global user.name "your user name"
 
 init #initialize a Git repository 
 
-log #show a repository's commit history 
+log #show a repository's commit history, including authors and commit messages
 
-pull #fetch data from a remote repository 
+pull #fetch data from a remote repository and merge it with your commands
 
-push #add committed changes to a remote Git repository, such as GitHub
+push #add committed changes to remote Git repository 
+#ex.) git push origin main pushes changes to the main branch of the repository 
 
-rm #remove files
 
-#powershell commands
+
+#Powershell commands
 
 cd #change working directory 
+
+cp #copies an item from one location to another
+
+mv #moves and item from one location to another
+
+rm #deletes file 
